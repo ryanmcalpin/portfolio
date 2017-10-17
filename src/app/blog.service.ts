ca738736038ddaf5dba7e0f07b9f0867ad98059d
Ryan McAlpin <rhmcalpin@gmail.com>
2017-10-17 16:05:50 -0700

    style blog card, change blog detail body tag to pre, style blog detail

--- src/app/blog.service.ts
+++ src/app/blog.service.ts
@@ -9,12 +9,14 @@ export class BlogService {
       {
         title: "This is the Title",
         lede: "This is the lede.",
-        body: "This is the body. It's very insightful."
+        body: "\tThis is the body. It's very insightful. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n\n\tLorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        date: "October 17, 2017"
       },
       {
         title: "This is the Other Title",
         lede: "This is other the lede.",
-        body: "This is the other body. It's very insightful."
+        body: "\tThis is the other body. It's very insightful. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        date: "October 17, 2017"
       }
     ]
   }
