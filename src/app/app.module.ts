cdcae40815b5297aad086a4a8f2ec7634fb8e0e0
Ryan McAlpin <rhmcalpin@gmail.com>
2017-10-22 17:02:36 -0700

    branch mngt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RoutingModule } from './routing/routing.module';
 
+import { BlogService } from './blog.service';
+
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 import { ProjectsComponent } from './projects/projects.component';
@@ -9,8 +11,6 @@ import { ContactComponent } from './contact/contact.component';
 import { FooterComponent } from './footer/footer.component';
 import { BlogComponent } from './blog/blog.component';
 import { BlogDetailComponent } from './blog-detail/blog-detail.component';
-
-import { BlogService } from './blog.service';
 
 @NgModule({
   declarations: [
