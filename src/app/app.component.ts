15a18107bb483c0157f01fa4eeee282d2ee90184
Ryan McAlpin <rhmcalpin@gmail.com>
2017-10-31 21:48:07 -0700

    refactor route title logic, display blog title breadcrumb

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { NgClass } from '@angular/common';
 import { Router } from '@angular/router';
 import 'rxjs/add/operator/switchMap';
 
+import { BlogService } from './blog.service'
+
 
 @Component({
   selector: 'app-root',
@@ -12,15 +14,32 @@ import 'rxjs/add/operator/switchMap';
 export class AppComponent {
   title = 'app';
   collapsedState = 'shown';
+  routeTitle: string = '';
 
-  constructor(private router: Router) { }
+  constructor(private router: Router,
+              private blogService: BlogService) { }
 
   checkNav(nav): string {
-    return nav === this.router.url ? 'active' : '';
+    if (nav === '/blog' && this.router.url.slice(0, 5) === '/blog' && this.router.url.length > 5) {
+      var post = this.blogService.getPost(this.router.url.slice(6));
+      this.routeTitle = post.title;
+      return 'active';
+    }
+
+    if (nav === this.router.url) {
+      this.routeTitle = nav.charAt(1).toUpperCase() + nav.slice(2);
+      return 'active';
+    } else {
+      return '';
+    }
   }
 
   checkCollapsed() {
     var el = document.getElementById('nav-button');
-    this.collapsedState = el.classList.contains('collapsed') ? 'hidden' : 'shown';
+    if (this.router.url.length > 5 && this.router.url.slice(0, 5) === '/blog') {
+      this.collapsedState = el.classList.contains('collapsed') ? 'hidden title' : 'shown title';
+    } else {
+      this.collapsedState = el.classList.contains('collapsed') ? 'hidden' : 'shown';
+    }
   }
 }
